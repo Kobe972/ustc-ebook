ff49b2b742792dc806754d857abac1d86235f76a
徐亦昶 <71507738+Kobe972@users.noreply.github.com>
2021-09-29 22:27:40 +0800

    Add files via upload

---
+++ 中华数字书苑/在线阅读提取pdf.py
@@ -0,0 +1,32 @@
+import requests
+import re
+import os
+import shutil
+import json
+import img2pdf
+url=input('输入阅读器页面地址：')
+total_page=int(input('输入总页数：'))
+center=re.search('(\\?metaid=.*)&page=.*?(&.*)',url,re.S)
+center=center.group(1)+center.group(2)
+base='http://cebxol.apabiedu.com/api/getservice'+center+'&width=300&height=500&ServiceType=imagepage&page='
+if not os.access('tmp',os.F_OK):
+    os.mkdir('tmp')
+print('jpg下载中……')
+for i in range(1,total_page+1):
+    url=base+str(i)
+    page=requests.get(url)
+    if(len(page.content)<2000):
+        i-=1
+        continue
+    with open('.\\tmp\\'+str(i)+'.jpg','wb') as fd:
+        fd.write(page.content)
+target_path = '.\\tmp' #收集下载好的片段
+jpg_lst = [int(f[:-4]) for f in os.listdir(target_path) if f.endswith('.jpg')]
+jpg_lst.sort()
+print('pdf合成中……') #将下载的每一页合并
+for i in range(0,len(jpg_lst)):
+    jpg_lst[i]=str(jpg_lst[i])+'.jpg'
+jpg_lst = [os.path.join(target_path, filename) for filename in jpg_lst]
+with open(".\\final.pdf", "wb") as f:
+    f.write(img2pdf.convert(jpg_lst))
+shutil.rmtree('.\\tmp') #删除临时文件
--- 超星电子图书/网页阅读下载.py
+++ 超星电子图书/网页阅读下载.py
@@ -12,11 +12,6 @@ title=result.group(1)
 pages=json.loads(result.group(2))
 jpgPath=result.group(3)
 base='http://img.sslibrary.com'+jpgPath #文件基址
-headers={
-    'Referer':'http://img.sslibrary.com/',
-    'Host':'npng1.5read.com',
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31'
-    }
 if not os.access('tmp',os.F_OK):
     os.mkdir('tmp')
 print('jpg下载中……')
